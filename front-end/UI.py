77c2dc46ec819faffcbb5b5cdf5c5bb1da1cd20c
jamestang12 <homing.tang@ryerson.ca>
2020-05-15 22:36:48 -0400

    Main Page UI Created

--- front-end/UI.py
+++ front-end/UI.py
@@ -15,4 +15,26 @@ logo_image = ImageTk.PhotoImage(Image.open(
 logo_label = tk.Label(root, image=logo_image)
 logo_label.place(relwidth=1, relheight=0.4)
 
+createContact_image = ImageTk.PhotoImage(
+    file='./image/create.png')  # image for create contact button
+
+verifyContact_image = ImageTk.PhotoImage(
+    file='./image/verify.png')  # image for verify contact button
+
+readContact_image = ImageTk.PhotoImage(  # image for read contact button
+    file='./image/read.png'
+)
+
+createContact_button = tk.Button(
+    image=createContact_image, bd=0, font=40)  # craete contact button
+createContact_button.place(x=300, y=290)
+
+verifyContact_button = tk.Button(
+    image=verifyContact_image, bd=0)  # verify contact button
+verifyContact_button.place(x=300, y=375)
+
+readContact_button = tk.Button(
+    image=readContact_image, bd=0)  # read contact button
+readContact_button.place(x=300, y=460)
+
 root.mainloop()
